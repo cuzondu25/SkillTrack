3d3d9821180d9280d28fd1eb356b7d46faaedcd4
cuzondu25 <cuzondu25@gmail.com>
2024-12-16 14:30:05 +0100

    frontend course enrollment implemented and tested

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import CourseList from './pages/CourseList';
+import EnrolledCourses from './pages/EnrolledCourses';
 
 function App() {
     return (
@@ -9,6 +11,8 @@ function App() {
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/courses" element={<CourseList />} />
+                <Route path="/enrolled-courses" element={<EnrolledCourses />} />
             </Routes>
         </Router>
     );
